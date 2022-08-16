a03cad5781f1c41e09a50c0b7fb14009f9901c86
agilyova <80471997+agilyova@users.noreply.github.com>
2022-08-16 22:06:02 +0300

    Level 1 (#2)
    * Added api
    
    * Debug
    
    * Debug
    
    * Debug
    
    * Debug
    
    * Debug
    
    * Debug
    
    * Some fixes
    
    * Some fixes
    
    * Fixed after review
    
    * Fixed after review. Added index to owner + movie, rewrite create movie method
    
    * Some fixes for integration with front

--- errors/error-constunts.js
+++ errors/error-constunts.js
@@ -4,11 +4,12 @@ module.exports.NOT_FOUND = 404;
 module.exports.CONFLICT = 409;
 module.exports.INTERNAL_SERVER_ERROR = 500;
 
-module.exports.WRONG_EMAIL_OR_PASSWORD = 'Неверные Пользователь или пароль';
+
+module.exports.WRONG_EMAIL_OR_PASSWORD = 'Вы ввели неправильный логин или пароль.';
 module.exports.NEED_AUTHORISATION = 'Необходима авторизация';
-module.exports.EMAIL_IS_TAKEN = 'Данный email уже занят';
+module.exports.EMAIL_IS_TAKEN = 'Пользователь с таким email уже существует';
 module.exports.MOVIE_ALREADY_IN_WISH = 'Фильм уже добавлен в избранное';
 module.exports.DELETE_FILM_FORBIDDEN = 'Нельзя удалить чужой фильм из избранного';
-module.exports.ROUTE_NOT_FOUND = 'Запрашиваемый роут не найден';
+module.exports.ROUTE_NOT_FOUND = 'Страница по указанному маршруту не найдена';
 module.exports.FILM_NOT_FOUND = 'Фильм не найден';
 module.exports.USER_NOT_FOUND = 'Пользователь не найден';
--- routes/auth.js
+++ routes/auth.js
@@ -4,7 +4,7 @@ const { createUser, login, logout } = require('../controllers/users');
 
 router.post('/signup', celebrate({
   body: Joi.object().keys({
-    email: Joi.string().required().email(),
+    email: Joi.string().required().email({ minDomainSegments: 1, tlds: { allow: false } }),
     password: Joi.string().required().min(5),
     name: Joi.string().trim().required().min(2)
       .max(30),
@@ -13,7 +13,7 @@ router.post('/signup', celebrate({
 
 router.post('/signin', celebrate({
   body: Joi.object().keys({
-    email: Joi.string().required().email(),
+    email: Joi.string().required().email({ minDomainSegments: 1, tlds: { allow: false } }),
     password: Joi.string().required(),
   }),
 }), login);
